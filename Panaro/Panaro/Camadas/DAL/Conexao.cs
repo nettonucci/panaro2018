dfbf63a9723d321b4819c0602d9ed3d4b83caca2
Netto Nucci <nettonucci@gmail.com>
2018-05-03 00:01:12 -0300

    Inicio
    camadas e conexao

---
+++ Panaro/Panaro/Camadas/DAL/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaro.Camadas.DAL
+{
+    public class Conexao
+    {
+        static string getConexao()
+        {
+            return @"Data Source=.\sqlexpress;Initial Catalog=panaro;Integrated Security=True";
+        }
+    }
+}
---
+++ Panaro/Panaro/Camadas/Model/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaro.Camadas.Model
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+    }
+}

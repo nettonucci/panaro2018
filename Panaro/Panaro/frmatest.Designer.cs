f2e1081858f9c98d20036b4d8a1f5cccfcaa273c
Leticia Viccioli <le.bviccioli@live.com>
2018-05-10 19:48:23 -0300

    Atualizações
    atualizações

--- Panaro/Panaro/frmatest.Designer.cs
+++ Panaro/Panaro/frmatest.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 257);
             this.Name = "frmatest";
             this.Text = "Atualizar estoque";
             this.ResumeLayout(false);
--- Panaro/Panaro/frmcontpag.Designer.cs
+++ Panaro/Panaro/frmcontpag.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(774, 449);
             this.Name = "frmcontpag";
             this.Text = "Contas a pagar";
             this.ResumeLayout(false);

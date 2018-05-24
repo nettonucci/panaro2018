9367459f13bd2aaaff72311eb41dee64aa806708
Netto <nettonucci@gmail.com>
2018-05-23 22:33:58 -0300

    sono
    sei la

--- Panaro/Panaro/frmconpro.Designer.cs
+++ Panaro/Panaro/frmconpro.Designer.cs
@@ -152,6 +152,7 @@
             this.button2.TabIndex = 12;
             this.button2.Text = "Sair";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // frmconpro
             // 

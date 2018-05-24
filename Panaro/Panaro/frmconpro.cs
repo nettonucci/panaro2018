053761e0fce0406779dddab6ba99ba3588c84d1e
Leticia Viccioli <le.bviccioli@live.com>
2018-05-23 22:06:21 -0300

    Consulta de Produtos
    formulario consulta de produtos

--- Panaro/Panaro/frmconpro.cs
+++ Panaro/Panaro/frmconpro.cs
@@ -16,5 +16,10 @@ namespace Panaro
         {
             InitializeComponent();
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
